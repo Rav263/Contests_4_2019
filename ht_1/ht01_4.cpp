9fa7c84611c7b2e8cf9d162353259b6df257a7c2
RAV263 <nickiforov.nik@gmail.com>
2019-05-08 18:19:16 +0300

    решена 4-я задача

--- ht_1/ht01_4.cpp
+++ ht_1/ht01_4.cpp
@@ -5,7 +5,7 @@
 #include <utility>
 #include <cstdlib>
 #include <vector>
-
+#include <iomanip>
 
 constexpr uint64_t low = 0;
 constexpr uint64_t high = 40;
@@ -33,13 +33,10 @@ int main(int argc, char *argv[]) {
     uint64_t thread_count = std::stoll(argv[1]);
     uint64_t iter_count = std::stoll(argv[2]);
 
-    std::cerr << thread_count << " " << iter_count << std::endl;
-
-    std::vector<std::future<std::pair<uint64_t, uint64_t>>> results;
+    std::vector<std::future<std::pair<uint64_t, uint64_t>>> results(thread_count);
 
     for (uint32_t i = 0; i < thread_count; i++) {
-        std::cerr << i << std::endl;
-        results.push_back(std::async(std::launch::async, count_points, iter_count, i));
+        results[i] = std::async(std::launch::async, count_points, iter_count, i);
     }
 
     uint64_t points_counter = 0;
@@ -48,11 +45,13 @@ int main(int argc, char *argv[]) {
 
     for (uint32_t i = 0; i < thread_count; i++) {
         results[i].wait();
-        points_counter += results[i].get().first;
-        good_points_counter += results[i].get().second;    
+        auto now_result = results[i].get();
+
+        points_counter += now_result.first;
+        good_points_counter += now_result.second;    
     }
 
     double pi = ((double) good_points_counter / (double) points_counter) * 4;
 
-    std::cout << pi << std::endl;
+    std::cout << std::setprecision(7) << pi << std::endl;
 }
